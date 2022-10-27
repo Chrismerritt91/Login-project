389a5c849d47c88c5dd8e2a74c1bbc475d63a45a
Chris Merritt <christopher.aa.merritt@gmail.com>
2022-10-27 16:16:34 -0500

    complete all but username check feat on registration page

--- src/main/java/com/example/loginproject/controller/registerController.java
+++ src/main/java/com/example/loginproject/controller/registerController.java
@@ -1,9 +1,21 @@
 package com.example.loginproject.controller;
 
+import com.example.loginproject.repositories.UserRepository;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class registerController {
 
+    private UserRepository usersDao;
+
+    public registerController(UserRepository usersDao){
+        this.usersDao = usersDao;
+    }
+
+    @GetMapping("/register")
+    public String registerNow(){
+        return "register";
+    }
 
 }

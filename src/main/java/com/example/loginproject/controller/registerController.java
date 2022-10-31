f7e674ede1e5c4b035b4f74a9380e17a447c961d
Chris Merritt <christopher.aa.merritt@gmail.com>
2022-10-31 15:44:37 -0500

    add styling to pages and create controller for user creation

--- src/main/java/com/example/loginproject/controller/registerController.java
+++ src/main/java/com/example/loginproject/controller/registerController.java
@@ -1,21 +1,37 @@
 package com.example.loginproject.controller;
 
+import com.example.loginproject.model.User;
 import com.example.loginproject.repositories.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class registerController {
 
     private UserRepository usersDao;
+    private PasswordEncoder passwordEncoder;
 
-    public registerController(UserRepository usersDao){
+    public registerController(UserRepository usersDao, PasswordEncoder passwordEncoder) {
         this.usersDao = usersDao;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @GetMapping("/register")
-    public String registerNow(){
+    public String registerNow(Model model){
+        model.addAttribute("user", new User());
         return "register";
+    }
+
+    @PostMapping("/user/create")
+    public String insertUser(@ModelAttribute User user){
+        String hash = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hash);
+        usersDao.save(user);
+        return "redirect:/";
     }
 
 }
